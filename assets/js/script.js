5aff3b21571655cafd41de1e38f332c31f38633b
Vadim <madvad137@gmail.com>
2025-10-10 15:19:51 +0300

    продолжаю верстать главную

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,8 +93,86 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   }
 
-    InitFAQItems(document.querySelectorAll('.footer__services-item'))
-    
-    InitFAQItems(document.querySelectorAll('.prices__item-titleBlock'))
-    InitTabs('.prices')
+  InitFAQItems(document.querySelectorAll('.footer__services-item'))
+  
+  InitFAQItems(document.querySelectorAll('.prices__item-titleBlock'))
+  InitTabs('.prices')
+
+  const howFastSelects = document.querySelectorAll('.howFast__formBlock-input')
+
+  if(howFastSelects.length){
+    howFastSelects.forEach((select) =>{
+      select.addEventListener('click',()=>{
+        const selectList = select.querySelector('.howFast__formBlock-list')
+        if(selectList){
+          selectList.classList.toggle('active')
+        }
+      })
+
+      const selectListItems = select.querySelectorAll('.howFast__formBlock-list .howFast__formBlock-select')
+      if(selectListItems.length){
+        selectListItems.forEach((item) =>{
+          item.addEventListener('click',() =>{
+            
+            const selectText = select.querySelector('.howFast__formBlock-text')
+            selectText.innerHTML = item.innerHTML
+          })
+        })
+      }
+    })
+  }
+
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+		let flagMap = false;
+		document.addEventListener('scroll', function() {
+			const blockMap = document.getElementById('yandex_map');
+      if(blockMap){
+        const posTop = blockMap.getBoundingClientRect().top;
+        if (typeof posTop !== "undefined" && posTop < window.innerHeight && !flagMap) {
+          if (!document.querySelector('[src="https://api-maps.yandex.ru/2.1/?apikey=&lang=ru_RU"]')) {
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://api-maps.yandex.ru/2.1/?apikey=&lang=ru_RU';
+            document.head.appendChild(script);
+          }
+          
+            setTimeout(() => {
+              ymaps.ready(init);
+            }, 1500);
+          
+
+          function init() {
+            var myMap = new ymaps.Map("yandex_map", {
+                center: [55.73109,37.621291],
+                zoom: 16
+              }, {
+                searchControlProvider: 'yandex#search',
+                
+              }),
+              myPlacemark = new ymaps.Placemark(myMap.getCenter());
+            myMap.geoObjects
+              .add(myPlacemark);
+            
+
+              myMap.controls.remove('routeButtonControl');   
+              myMap.controls.remove('routePanelControl');   
+              myMap.controls.remove('routeEditorControl');   
+              myMap.controls.remove('searchControl');
+              myMap.controls.remove('zoomControl');
+              myMap.controls.remove('geolocationControl');
+              myMap.controls.remove('trafficControl');
+              myMap.controls.remove('typeSelector');
+              myMap.controls.remove('fullscreenControl');
+              myMap.controls.remove('rulerControl');
+               myMap.behaviors.disable('scrollZoom')
+          }
+
+          flagMap = true;
+        }
+      }
+		});
+	});
